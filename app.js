46c96db1413837bebd40f21d71f10ceb104906e2
Joonas <joonasroska123@gmail.com>
2025-07-06 11:49:59 +0300

    removed excess route

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ import gpuRoutes from './routes/gpuRoutes.js'
 const app = express();
 
 app.use(express.json());
-app.use('/api/users', userRoutes);
 app.use('/api/cpus', cpuRoutes);
 app.use('/api/gpus', gpuRoutes);
 app.use('/api/price', priceRoutes);
