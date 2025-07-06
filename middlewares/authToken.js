1756c3df9817831093424a3dd471b186ef944f65
Joonas <joonasroska123@gmail.com>
2025-07-07 01:07:02 +0300

    edit

--- middlewares/authToken.js
+++ middlewares/authToken.js
@@ -1,22 +1,16 @@
 
 
 export default function authenticateToken(req, res, next) {
-    const API_TOKEN = process.env.ACCESS_TOKEN || "default-token";
-    const token = req.headers['authorization'];
-
-  console.log('Received token:', token);
-  console.log('local token', API_TOKEN)
+  const API_TOKEN = process.env.ACCESS_TOKEN || "default-token";
+  const token = req.headers['authorization'];
 
   if (!token) {
-    console.warn('No Authorization header sent');
     return res.status(403).json({ error: 'No token provided' });
   }
 
   if (token !== `Bearer ${API_TOKEN}`) {
-    console.warn('Invalid token:', token);
     return res.status(403).json({ error: 'Unauthorized: Invalid token' });
   }
 
-  console.log('Token validated successfully');
   next();
 }
